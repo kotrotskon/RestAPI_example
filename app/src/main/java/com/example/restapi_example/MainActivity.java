4ddfb1e6782360169d5829ec3eacf5b449a9ca5b
KotrotsKon <kotrotskon@users.noreply.github.com>
2022-07-21 18:25:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/restapi_example/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.restapi_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        final String URL = "https://www.google.com/test";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("RESPONSE", response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("RESPONSE", error.getMessage());
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
